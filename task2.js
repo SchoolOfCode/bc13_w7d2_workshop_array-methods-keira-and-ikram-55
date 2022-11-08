666d0ae0c12b54b057952f3ecbb3a576aa7d3605
Ikram-Zak <112779062+Ikram-Zak@users.noreply.github.com>
2022-11-08 13:02:15 +0000

    task 2

--- task2.js
+++ task2.js
@@ -8,4 +8,24 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter(filteredB)
+  console.log(bListAnimals);
+
+function filteredB(word){
+  return word.startsWith("b")
+}
+
+
+
+/*animals.filter((element) => filteredB(element))
+console.log(bListAnimals);
+
+
+}
+
+//start function
+// use .filter and add the function within it
+
+/*take in each word
+check if the word start with 'b'
+if so, add it to the new filtered array*/
