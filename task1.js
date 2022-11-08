ba94efaaddf25e90f083e0b5eecd6a9ff5c983cb
keirastanley <keiraestanley@gmail.com>
2022-11-08 12:25:12 +0000

    Simplified version of task1

--- task1.js
+++ task1.js
@@ -1,10 +1,20 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = cats.map(animal => capitalised(animal))
+//Version 1
+/*const capitalisedCats = cats.map(animal => capitalised(animal))
 console.log(capitalisedCats)
-function capitalised (word){
-return word.toUpperCase()
-}
 
+function capitalised (word){
+    return word.toUpperCase()
+}
+*/
+//Version 2
+/*const capitalisedCats = cats.map(function (element) {
+    return element.toUpperCase()
+})*/
+
+//Version 3
+const capitalisedCats = cats.map(element => element.toUpperCase())
+console.log(capitalisedCats)
 // first writing a basic function
 // instert the function into map
