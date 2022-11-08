bb232ae76422b8d828b8ebdf048403a26bc3d684
Ikram-Zak <112779062+Ikram-Zak@users.noreply.github.com>
2022-11-08 12:17:46 +0000

    task1

--- task1.js
+++ task1.js
@@ -1,3 +1,10 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(animal => capitalised(animal))
+console.log(capitalisedCats)
+function capitalised (word){
+return word.toUpperCase()
+}
+
+// first writing a basic function
+// instert the function into map
